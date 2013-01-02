dde9d10265a203bd6aee9b3de4fe8eb4459e03f9
Eric Wong <ericsyw@gmail.com>
2013-01-02 10:40:21 +0800

    Add shops and relate get API. move Coupon/Relate to Items module

--- lib/top_api.rb
+++ lib/top_api.rb
@@ -6,9 +6,11 @@ require "top_api/taobaoke"
 module TopApi
   METHODS = {
     :taobaoke_items_get => Taobaoke::Items::Get,
-    :taobaoke_coupon_get => Taobaoke::Coupon::Get,
-    :taobaoke_relate_get => Taobaoke::Relate::Get,
-    :taobaoke_report_get => Taobaoke::Report::Get
+    :taobaoke_items_coupon_get => Taobaoke::Items::Coupon::Get,
+    :taobaoke_items_relate_get => Taobaoke::Items::Relate::Get,
+    :taobaoke_report_get => Taobaoke::Report::Get,
+    :taobaoke_shops_get => Taobaoke::Shops::Get,
+    :taobaoke_shops_relate_get => Taobaoke::Shops::Relate::Get
   }
 
   class << self
--- lib/top_api/taobaoke.rb
+++ lib/top_api/taobaoke.rb
@@ -1,20 +1,20 @@
 module TopApi
   module Taobaoke
-    TaobaokeItemFields = [:num_iid,
-                          :nick,
-                          :title,
-                          :price,
-                          :item_location,
-                          :seller_credit_score,
-                          :click_url,
-                          :shop_click_url,
-                          :pic_url,
-                          :commission_rate,
-                          :commission,
-                          :commission_num,
-                          :commission_volume,
-                          :volume]
     module Items
+      TaobaokeItemFields = [:num_iid,
+                            :nick,
+                            :title,
+                            :price,
+                            :item_location,
+                            :seller_credit_score,
+                            :click_url,
+                            :shop_click_url,
+                            :pic_url,
+                            :commission_rate,
+                            :commission,
+                            :commission_num,
+                            :commission_volume,
+                            :volume]
       class Get < RestApi
         def initialize
           super
@@ -32,45 +32,45 @@ module TopApi
           end
         end
       end
-    end
 
-    module Coupon
-      class Get < RestApi
-        def initialize
-          super
-          @params[:method] = 'taobao.taobaoke.items.coupon.get'
-          @params[:pid] = @taobaoke_id
-          @params[:fields] = (TaobaokeItemFields | [:coupon_rate,
-            :coupon_price, :coupon_start_time,
-            :coupon_end_time, :shop_type]).join ','
-        end
+      module Coupon
+        class Get < RestApi
+          def initialize
+            super
+            @params[:method] = 'taobao.taobaoke.items.coupon.get'
+            @params[:pid] = @taobaoke_id
+            @params[:fields] = (TaobaokeItemFields | [:coupon_rate,
+              :coupon_price, :coupon_start_time,
+              :coupon_end_time, :shop_type]).join ','
+          end
 
-        def get options
-          request options do |body|
-            resp = body['taobaoke_items_coupon_get_response']
-            nums = resp['total_results']
-            items = resp['taobaoke_items']['taobaoke_item']
-            yield(nums, items)
+          def get options
+            request options do |body|
+              resp = body['taobaoke_items_coupon_get_response']
+              nums = resp['total_results']
+              items = resp['taobaoke_items']['taobaoke_item']
+              yield(nums, items)
+            end
           end
         end
       end
-    end
 
-    module Relate
-      class Get < RestApi
-        def initialize
-          super
-          @params[:method] = 'taobao.taobaoke.items.relate.get'
-          @params[:pid] = @taobaoke_id
-          @params[:fields] = TaobaokeItemFields.join ','
-        end
+      module Relate
+        class Get < RestApi
+          def initialize
+            super
+            @params[:method] = 'taobao.taobaoke.items.relate.get'
+            @params[:pid] = @taobaoke_id
+            @params[:fields] = TaobaokeItemFields.join ','
+          end
 
-        def get options
-          request options do |body|
-            resp = body['taobaoke_items_relate_get_response']
-            num = resp['total_results']
-            items = resp['taobaoke_items']['taobaoke_item']
-            yield(num, items)
+          def get options
+            request options do |body|
+              resp = body['taobaoke_items_relate_get_response']
+              num = resp['total_results']
+              items = resp['taobaoke_items']['taobaoke_item']
+              yield(num, items)
+            end
           end
         end
       end
@@ -106,6 +106,55 @@ module TopApi
             num = resp['total_results']
             reports = resp['taobaoke_report']['taobaoke_report_members']['taobaoke_report_member']
             yield(num, reports)
+          end
+        end
+      end
+    end
+
+    module Shops
+      TaobaokeShopFields = [:seller_nick,
+                            :user_id,
+                            :shop_title,
+                            :click_url,
+                            :commission_rate,
+                            :seller_credit,
+                            :shop_type,
+                            :total_auction,
+                            :auction_count]
+      class Get < RestApi
+        def initialize
+          super
+          @params[:method] = 'taobao.taobaoke.shops.get'
+          @params[:pid] = @taobaoke_id
+          @params[:fields] = TaobaokeShopFields.join ','
+        end
+
+        def get options
+          request options do |body|
+            resp = body['taobaoke_shops_get_response']
+            num = resp['total_results']
+            shops = resp['taobaoke_shops']['taobaoke_shop']
+            yield(num, shops)
+          end
+        end
+      end
+
+      module Relate
+        class Get < RestApi
+          def initialize
+            super
+            @params[:method] = 'taobao.taobaoke.shops.relate.get'
+            @params[:pid] = @taobaoke_id
+            @params[:fields] = TaobaokeShopFields.join ','
+          end
+
+          def get options
+            request options do |body|
+              resp = body['taobaoke_shops_relate_get_response']
+              num = resp['total_results']
+              shops = resp['taobaoke_shops']['taobaoke_shop']
+              yield(num, shops)
+            end
           end
         end
       end
